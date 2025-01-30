06b0aa8874ef50f09f585dc794718d9ef68c1629
Clement Cabus <chummycabus@gmail.com>
2025-01-30 23:29:31 +0800

    LEARNING ROUTING AND USING LARAVEL CMD COMMANDS - using "php artisan make:view FILE NAME" - use cd and lock in to the main file folder

---
+++ public/hello.php
@@ -0,0 +1,4 @@
+<?php 
+// even though this file is in the public directory,
+// it is not accessible from the browser
+echo "Hello World!";
--- routes/web.php
+++ routes/web.php
@@ -5,3 +5,14 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('/hello', function () {
+    return view('hello');
+});
+
+// you can use these to either link to views
+
+// or to create pages that don't have a view
+// example: 
+//Route::get('/hello', function ()
+// { return 'Hello World'; });
